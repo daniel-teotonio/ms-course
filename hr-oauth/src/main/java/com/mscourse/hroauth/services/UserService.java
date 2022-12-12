9b5347df9df714d0c10cca67b04354dd2e4c1488
DanielTeotonio <danielcarlos_mt@hotmail.com>
2022-12-12 14:41:40 -0300

    Login, JWT token

--- hr-oauth/src/main/java/com/mscourse/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/mscourse/hroauth/services/UserService.java
@@ -6,10 +6,13 @@ import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -26,4 +29,14 @@ public class UserService {
         return user;
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userFeignclients.findByEmail(username).getBody();
+        if(user == null){
+            logger.error("Email not found: " + username);
+            throw new UsernameNotFoundException("User not found");
+        }
+        logger.info("User found: " + username);
+        return user;
+    }
 }
